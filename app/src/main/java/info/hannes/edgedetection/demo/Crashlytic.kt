a71b20f4e9f8d09ad4f7f133cb7d6f69952dd4cd
Hannes Achleitner <hannes.software@gmx.at>
2022-11-16 06:34:09 +0100

    Fix debug build

---
+++ app/src/main/java/info/hannes/edgedetection/demo/Crashlytic.kt
@@ -0,0 +1,7 @@
+package info.hannes.edgedetection.demo
+
+import android.content.ContentResolver
+
+object Crashlytic {
+    fun init(@Suppress("UNUSED_PARAMETER") contentResolver: ContentResolver) = Unit
+}
