f1fa6ddcae5ca95ff4c2d730a41100246074a1e2
Hannes Achleitner <hannes.software@gmx.at>
2022-11-16 10:10:08 +0100

    Camera test

--- app/src/androidTest/java/info/hannes/edgedetection/demo/SmokeTest.kt
+++ app/src/androidTest/java/info/hannes/edgedetection/demo/SmokeTest.kt
@@ -1,5 +1,6 @@
 package info.hannes.edgedetection.demo
 
+import android.Manifest
 import android.util.Log
 import androidx.test.core.app.takeScreenshot
 import androidx.test.core.graphics.writeToTestStorage
@@ -8,6 +9,7 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.screenshot.captureToBitmap
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestName
@@ -22,6 +24,13 @@ class SmokeTest {
     // a handy JUnit rule that stores the method name, so it can be used to generate unique screenshot files per test method
     @get:Rule
     var nameRule = TestName()
+
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
 
     @Test
     fun smokeTestSimplyStart() {
