4eefa740fa5945419748b7619d33d71bd1882eca
Hannes Achleitner <hannes.software@gmx.at>
2023-06-04 14:48:12 +0200

    Remove Crashlytics

--- app/src/main/java/info/hannes/edgedetection/demo/Crashlytic.kt
+++
@@ -1,7 +0,0 @@
-package info.hannes.edgedetection.demo
-
-import android.content.ContentResolver
-
-object Crashlytic {
-    fun init(@Suppress("UNUSED_PARAMETER") contentResolver: ContentResolver) = Unit
-}
