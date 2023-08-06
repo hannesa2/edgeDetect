bfbda17872ab3de90349825f688c27d762c78cb0
Hannes Achleitner <hannes.software@gmx.at>
2023-08-06 08:04:24 +0200

    Improve decrypt

--- signing/decrypt.sh
+++ signing/decrypt.sh
@@ -14,15 +14,21 @@ else
    echo "\$CRYPT_PASS available"
 fi
 
+openssl version
+
 pushd signing
 
 # to encrypt
-#openssl aes-256-cbc -a -salt -k "$CRYPT_PASS" -in release.keystore -out release.keystore.enc
-#openssl aes-256-cbc -a -salt -k "$CRYPT_PASS" -in ./app/google-services.json -out ./app/google-services.json.enc
+#openssl aes-256-cbc -a -salt -k "$CRYPT_PASS" -in ./signing/release.keystore -out release.keystore.enc
+#openssl aes-256-cbc -salt -pbkdf2 -k "$CRYPT_PASS" -in ./app/google-services.json -out ./app/google-services.json.enc
 
 # Ubuntu 18.04 (openssl 1.1.0g+) needs -md md5
 # https://askubuntu.com/questions/1067762/unable-to-decrypt-text-files-with-openssl-on-ubuntu-18-04/1076708
-openssl aes-256-cbc -a -d -md md5 -k "$CRYPT_PASS" -in release.keystore.enc -out release.keystore
-openssl aes-256-cbc -a -d -md md5 -k "$CRYPT_PASS" -in ../app/google-services.json.enc -out ../app/google-services.json
+echo release.keystore
+echo ================
+openssl aes-256-cbc -a -d -pbkdf2 -k "$CRYPT_PASS" -in release.keystore.enc -out release.keystore
+echo google-services.json
+echo ====================
+openssl aes-256-cbc -d -pbkdf2 -k "$CRYPT_PASS" -in ../app/google-services.json.enc -out ../app/google-services.json
 
 popd 1>/dev/null
