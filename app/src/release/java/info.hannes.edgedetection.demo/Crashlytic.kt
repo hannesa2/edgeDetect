6eff78fd6fa6b222f98644d7c20b47642a980e27
Hannes Achleitner <hannes.software@gmx.at>
2025-06-17 14:18:06 +0200

    VersionName from OpenCV

--- app/src/release/java/info.hannes.edgedetection.demo/Crashlytic.kt
+++ app/src/release/java/info.hannes.edgedetection.demo/Crashlytic.kt
@@ -5,6 +5,7 @@ import android.content.ContentResolver
 import android.provider.Settings
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import info.hannes.crashlytic.CrashlyticsTree
+import org.opencv.android.OpenCVLoader
 import timber.log.Timber
 
 object Crashlytic {
@@ -12,6 +13,6 @@ object Crashlytic {
     fun init(contentResolver: ContentResolver) {
         Timber.plant(CrashlyticsTree(Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)))
         FirebaseCrashlytics.getInstance().setCustomKey("VERSION_NAME", BuildConfig.VERSION_NAME)
-//        FirebaseCrashlytics.getInstance().setCustomKey("OpenCV", org.opencv.BuildConfig.VERSION_NAME)
+        FirebaseCrashlytics.getInstance().setCustomKey("OpenCV", OpenCVLoader.OPENCV_VERSION)
     }
 }
